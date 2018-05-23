e5c9674d0700041365615c75b480653ddb085e32
Antonis <antonispat10@gmail.com>
2018-05-23 18:25:14 +0300

    creating components,services,routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule, } from '@angular/core';
+import { Routes,RouterModule } from '@angular/router';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { ContactItemComponent } from './contacts/contact-list//contact-item/contact-item.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+
+
+
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/contacts', pathMatch: 'full' },
+  { path: 'contacts', component: ContactsComponent, children: [
+      { path: '', component: ContactsComponent },
+      { path: 'new', component: ContactEditComponent },
+      { path: ':id', component: ContactDetailComponent },
+      { path: ':id/edit', component: ContactEditComponent }
+  ]},
+
+];
+
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
